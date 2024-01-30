05be8dba3a04224b854db73f3e42eaf53b428371
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-01-30 22:21:02 +0000

    Adding react router.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/components/LandingPage";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="" element={<LandingPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
