82478d29c10ecc0a90c7b6b4088965bb4a7a9740
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-01-31 20:44:34 +0000

    Visual improvements to Auth component.

--- frontend/src/pages/Auth/components/Auth.jsx
+++ frontend/src/pages/Auth/components/Auth.jsx
@@ -1,28 +1,69 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 function Auth() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We&apos;ll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container
+      className="align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Row className="justify-content-center mb-4 mt-5">
+        <Col className="text-center">
+          <h1 className="display-5">AI-CV Generator</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={14} sm={10} md={8} lg={6} className="mx-auto">
+          <Card className="p-4 shadow">
+            <Card.Body>
+              <Nav variant="tabs" defaultActiveKey="login" className="mb-3">
+                <Nav.Item>
+                  <Nav.Link eventKey="login" as={Link} to="/login">
+                    Login
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/sign-up">
+                    Sign Up
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+              <Form>
+                <Form.Group className="mb-3" controlId="formEmail">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Link
+                    to="/request-password-reset/"
+                    className="text-decoration-none"
+                  >
+                    Forgot Password?
+                  </Link>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
