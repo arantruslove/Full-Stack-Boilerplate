5e5b750129a8d9694aa89758213292c448a4ebe2
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-01-31 19:43:48 +0000

    Adding a basic authentication page.

--- frontend/src/pages/Auth/components/Auth.jsx
+++ frontend/src/pages/Auth/components/Auth.jsx
@@ -1,5 +1,29 @@
-function Auth({ isLogin }) {
-  return <>{isLogin ? <h1>Login Page</h1> : <h1>Signup Page</h1>}</>;
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function Auth() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We&apos;ll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 export default Auth;
