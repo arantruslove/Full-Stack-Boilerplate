22b16db502fb870b0081699cd7c8937b3a190ffa
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-01-31 19:03:10 +0000

    Setting up basic authentication folder structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/components/LandingPage";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import AuthContainer from "./pages/Auth/containers/AuthContainer";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="" element={<LandingPage />} />
+        <Route path="/login" element={<AuthContainer />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/Auth/components/Auth.jsx
@@ -0,0 +1,5 @@
+function Auth() {
+  return <h1>This is Authentication!</h1>;
+}
+
+export default Auth;
