479541310b92bf5029334c2e5d0ecb5b0a06976e
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-02-01 19:43:14 +0000

    Adding a simple test api endpoint in accounts app.

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+@api_view(["GET"])
+def test(request):
+    """Simple view to test endpoint."""
+    data = {"Response": "This is the first API response."}
+
+    return Response(data)
