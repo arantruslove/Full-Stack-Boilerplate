1a5c2d6bd03cb337117cbc7561551b190d1055a5
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-02-07 20:00:58 +0000

    Adding an endpoint for verifiying email address.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,4 +3,5 @@ from accounts import views
 
 urlpatterns = [
     path("sign-up/", views.sign_up, name="sign_up"),
+    path("verify-email/", views.verify_email, name="verify_email"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.db import transaction
+from rest_framework import status
 
 from accounts.serializers import UserSerializer
 from accounts.email import send_verification_email
+from accounts.models import EmailVerification
 
 
 @api_view(["POST"])
@@ -20,8 +22,33 @@ def sign_up(request):
             return Response({"error": str(e)}, status=500)
 
         return Response(
-            {"Response": "User signed up successfully.", "User": serializer.data},
+            {"response": "User signed up successfully.", "User": serializer.data},
             status=201,
         )
     else:
         return Response(serializer.errors, status=400)
+
+
+@api_view(["POST"])
+def verify_email(request):
+    """Verifies a users account by setting is_active = True."""
+    try:
+        with transaction.atomic():
+            token = request.data["token"]
+
+            # Setting the user as active and deleting its EmailVerification instance
+            email_verification = EmailVerification.objects.get(token=token)
+            user = email_verification.user
+            user.is_active = True
+            user.save()
+            email_verification.delete()
+
+            return Response(
+                {"response": "Account successfully verified."},
+                status=status.HTTP_200_OK,
+            )
+
+    except EmailVerification.DoesNotExist:
+        return Response(
+            {"error": "Invalid verification token."}, status=status.HTTP_404_NOT_FOUND
+        )
