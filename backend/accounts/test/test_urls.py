0ba2418396b72ccec871ab0dd654036004235468
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-02-07 20:40:37 +0000

    Adding test for verify_email endpoint.

--- backend/accounts/test/test_urls.py
+++ backend/accounts/test/test_urls.py
@@ -1,11 +1,13 @@
 from django.urls import reverse
 from rest_framework.test import APIClient
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
 
 from accounts.models import User, EmailVerification
 
 
 class SignUp(TestCase):
+    """Testing the sign-up endpoint."""
 
     def setUp(self):
         self.client = APIClient()
@@ -44,3 +46,33 @@ class SignUp(TestCase):
         user = User.objects.get(email="newuser@example.com")
         email = EmailVerification.objects.get(user=user)
         self.assertEqual(email.user, user)
+
+
+class VerifyEmail(TestCase):
+    """Testing the verify-email endpoint."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse("verify_email")
+
+        # Signing up a user
+        data = {
+            "email": "newuser@example.com",
+            "password": "testpassword123",
+        }
+        sign_up_url = reverse("sign_up")
+        self.client.post(sign_up_url, data, format="json")
+        self.user = User.objects.get(email="newuser@example.com")
+
+        verification = EmailVerification.objects.get(user=self.user)
+        self.token = verification.token
+
+    def test_successful_email_verification(self):
+        data = {"token": self.token}
+        response = self.client.post(self.url, data, format="json")
+        self.user.refresh_from_db()
+
+        with self.assertRaises(EmailVerification.DoesNotExist):
+            EmailVerification.objects.get(user=self.user)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.user.is_verified, True)
