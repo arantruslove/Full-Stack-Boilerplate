27bb7b43d24073844c0583e3c0c41caf02d26311
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-04 09:21:33 +0100

    Removing test.js.

--- test.js
+++
@@ -1,7 +0,0 @@
-function objectToQueryString(params) {
-  return Object.keys(params)
-    .map(
-      (key) => encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
-    )
-    .join("&");
-}
