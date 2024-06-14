ec0d7fe9704998b07db95107bc368b4712a74bcd
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-14 14:55:04 +0100

    Adding database backup and encryption.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -229,17 +229,20 @@ resource "aws_security_group" "rds_security_group" {
 
 # Creating postgresql database instance
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "16.3"
-  instance_class       = "db.t3.micro"
-  db_name              = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  parameter_group_name = "default.postgres16"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  engine_version          = "16.3"
+  instance_class          = "db.t3.micro"
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  parameter_group_name    = "default.postgres16"
+  publicly_accessible     = true
+  skip_final_snapshot     = true
+  storage_encrypted       = true
+  backup_retention_period = 7
+  backup_window           = "02:00-03:00" # When fewest users are expected to be online
 
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_security_group.id]
